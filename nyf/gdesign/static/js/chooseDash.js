88c4135421bb2d856a81307c4121fd50e0459b41
nyf <2547546731@qq.com>
2015-05-30 14:14:08 +0800

    fixbugs

--- nyf/gdesign/static/js/chooseDash.js
+++ nyf/gdesign/static/js/chooseDash.js
@@ -66,6 +66,7 @@
 	  //渲染数据
             
 		function  diffusionRate(){
+
 		  //扩散速度图
 		     $('#definedDash').highcharts({
 			title: {
@@ -204,11 +205,13 @@
 //向后台请求数据
 function requestData(){        
 		 requestType=0        
-                 windSpeed=null
+                          windSpeed=null
 		 dischargeSpeed=null
 		 $("#windSpeed")[0].childNodes[1].value=''
 		 $("#dischargeSpeed")[0].childNodes[1].value=''
-		//输入验证
+		 xAxis_categories = []
+		 full_series = []
+		/*//输入验证
 		 dict={'dashId':dashId,'requestType':requestType,'default_windSpeed':default_windSpeed,'default_dischargeSpeed':default_dischargeSpeed}
 		 //测试ajax请求
 		 $.post('/request_simulaton_data/',dict,
@@ -229,7 +232,36 @@ function requestData(){
 						ConcentrationCompare()
 					}
 			      }
-		 },'json')
+		 },'json')*/
+		obj = {'categories':['100', '200', '300', '400', '500', '600','700', '800', '900', '1000', '1100', '1200'],'series':[{'data':[],'name':''}]}
+		if(dashId == 0){
+			obj.series[0].data =  cal_single_rate_change(5,110)
+			obj.series[0].name = '风速'+5+'m/s'
+			xAxis_categories=obj.categories
+			full_series=obj.series
+			diffusionRate()
+		}
+		else if(dashId == 1){
+			obj.series[0].data =  cal_single_rate_change(5,110)
+			obj.series[0].name = '风速'+5+'m/s,泄放速度'+110+'m/s'
+			xAxis_categories=obj.categories
+			full_series=obj.series
+			rateCompare()
+		}
+		else if(dashId == 2){
+			obj.series[0].data =  cal_single_concentration_change(5,110)
+			obj.series[0].name = '泄放速度'+110+'m/s'
+			xAxis_categories=obj.categories
+			full_series=obj.series
+			diffusionConcentration()
+		}
+		else if(dashId == 3){
+			obj.series[0].data =  cal_single_concentration_change(5,110)
+			obj.series[0].name ='风速'+5+'m/s,泄放速度'+110+'m/s'
+			xAxis_categories=obj.categories
+			full_series=obj.series
+			ConcentrationCompare()
+		}
 }
 
 function simulationRequestData(){
@@ -242,7 +274,7 @@ function simulationRequestData(){
 		 	if(simulation_dischargeSpeed){
 				dict['simulation_dischargeSpeed']=simulation_dischargeSpeed
 			}
-			//测试ajax请求
+			/*//测试ajax请求
 			$.post('/request_simulaton_data/',dict,
 			function(data){
 			       if(data){
@@ -261,7 +293,36 @@ function simulationRequestData(){
 						ConcentrationCompare()
 					}
 			      }
-			},'json')
+			},'json')*/
+			obj = {'categories':['100', '200', '300', '400', '500', '600','700', '800', '900', '1000', '1100', '1200'],'series':[{'data':[],'name':''}]}
+			if(dashId == 0){
+				obj.series[0].data =  cal_single_rate_change(simulation_windSpeed,110)
+				obj.series[0].name = '风速'+simulation_windSpeed+'m/s'
+				xAxis_categories=obj.categories
+				full_series.push(obj.series[0])
+				diffusionRate()
+			}
+			else if(dashId == 1){
+				obj.series[0].data =  cal_single_rate_change(simulation_windSpeed,simulation_dischargeSpeed)
+				obj.series[0].name = '风速'+simulation_windSpeed+'m/s,泄放速度'+simulation_dischargeSpeed+'m/s'
+				xAxis_categories=obj.categories
+				full_series.push(obj.series[0])
+				rateCompare()
+			}
+			else if(dashId == 2){
+				obj.series[0].data =  cal_single_concentration_change(10,simulation_dischargeSpeed)
+				obj.series[0].name = '泄放速度'+simulation_dischargeSpeed+'m/s'
+				xAxis_categories=obj.categories
+				full_series.push(obj.series[0])
+				diffusionConcentration()
+			}
+			else if(dashId == 3){
+				obj.series[0].data =  cal_single_concentration_change(simulation_windSpeed,simulation_dischargeSpeed)
+				obj.series[0].name = '风速'+simulation_windSpeed+'m/s,泄放速度'+simulation_dischargeSpeed+'m/s'
+				xAxis_categories=obj.categories
+				full_series.push(obj.series[0])
+				ConcentrationCompare()
+			}
 		}
 	}
 //输入验证
@@ -306,7 +367,35 @@ function check(){
 
 
 
+function cal_syngx(A,B,s){
+    return A * Math.log(s) + B
+}
 
+//计算扩散速度
+function cal_single_rate_change(wing_rate,xiefang_rate){
+    Q = wing_rate/xiefang_rate
+    A = 0.159874 + 0.960923 * Q -1.24869* Q * Q + 0.3728 * Q * Q * Q
+    B = -0.478332 + 0.0418951 * Q + 0.153298 * Q * Q
+    data = []
+    data = [0,0,0,0,0,0,0,0,0,0,0,0]
+    for(var i =1;i <=12;i++){
+        data[i-1] = cal_syngx(A,B,i*100)
+    }
+    return data
+}
+
+//计算扩散浓度
+function cal_single_concentration_change(wing_rate,xiefang_rate){
+    Q = wing_rate/xiefang_rate
+    A =  -0.0161965+0.00543481*Q-0.00128631*Q*Q
+    B =  0.183894*Math.exp(-0.38484*Q)
+    data = []
+    data = [0,0,0,0,0,0,0,0,0,0,0,0]
+    for(var i =1;i <=12;i++){
+        data[i-1] = cal_syngx(A,B,i*100)
+    }
+    return data
+}
 
 
 
